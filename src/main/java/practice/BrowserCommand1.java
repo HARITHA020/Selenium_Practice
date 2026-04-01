85292cfa50542c01efed3c475793313724983eee
haritha <haritharavichandran11@gmail.com>
2026-04-01 13:50:40 +0530

    new changes now

--- src/main/java/practice/BrowserCommand1.java
+++ src/main/java/practice/BrowserCommand1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-S
+
 public class BrowserCommand1 {
 
 	public static void main(String[] args) {
